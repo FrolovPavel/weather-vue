583e9a78030b92d406265e2ddcd97c59d34eb9c5
Pavel Frolov <Frolov-Works@yandex.ru>
2021-11-08 01:08:56 +0300

    search in process

--- src/components/pages/PageWeather.vue
+++ src/components/pages/PageWeather.vue
@@ -6,31 +6,35 @@
         />
         <br>
         <CardInfoComponent></CardInfoComponent>
+        <br>
+        <SearchComponent></SearchComponent>
     </div>
 </template>
 
 <script>
-    import ButtonComponent from "../ui/Button";
-    import SelectComponent from "../ui/Select";
-    import {mapState} from "vuex";
-    import CardInfoComponent from "../blanks/CardInfo";
-    export default {
-        data: () => ({
-            test: false,
-        }),
-        components: {
-            ButtonComponent,
-            SelectComponent,
-            CardInfoComponent
-        },
-        computed: {
-            ...mapState({
-                selectOptionsWeather: state => state.selectOptionsWeather
-            })
-        },
-        mounted() {
-            console.log(this.selectOptionsWeather)
-        }
+import ButtonComponent from "../ui/Button";
+import SelectComponent from "../ui/Select";
+import {mapState} from "vuex";
+import CardInfoComponent from "../blanks/CardInfo";
+import SearchComponent from "../ui/Search";
+export default {
+    data: () => ({
+        test: false,
+    }),
+    components: {
+        ButtonComponent,
+        SelectComponent,
+        CardInfoComponent,
+        SearchComponent
+    },
+    computed: {
+        ...mapState({
+            selectOptionsWeather: state => state.selectOptionsWeather
+        })
+    },
+    mounted() {
+        console.log(this.selectOptionsWeather)
     }
+}
 </script>
 
--- src/components/ui/Search.vue
+++ src/components/ui/Search.vue
@@ -1,11 +1,48 @@
 <template>
-    <div></div>
+    <div class="search">
+        <input type="text"
+            v-model="query"
+
+        >
+    </div>
 </template>
 
 <script>
-    export default {
-        name: "search-component"
+
+
+export default {
+    name: "search-component",
+    data:() => ({
+        query: ''
+    }),
+    components: {
+
+    },
+    methods: {
+        test() {
+            const url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address";
+            const token = "ab237855f0c699002d2157207ddd43736f6c9a9a";
+
+
+            let options = {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                    "Authorization": "Token " + token
+                },
+                body: JSON.stringify({query: this.query})
+            }
+
+            fetch(url, options)
+                .then(response => response.text())
+                .then(result => console.log(result))
+                .catch(error => console.log("error", error));
+        }
+
     }
+}
 </script>
 
 <style>
