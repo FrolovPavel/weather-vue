334324e4241d80873b7e627bfe094559ab0527a0
Pavel Frolov <Frolov-Works@yandex.ru>
2021-11-08 21:14:30 +0300

    cardInfoBig rdy, layout all in process

--- src/components/blanks/CardInfoBig.vue
+++ src/components/blanks/CardInfoBig.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="card-info-big">
-
+        <div class="card-info-big__wrapper">
+            <h1 class="card-info-big__title">Волгоград, RU</h1>
+            <p class="card-info-big__subtitle">туман</p>
+            <div class="card-info-big__img">
+                <img src="@/assets/images/01d.svg" alt="weatherIcon">
+            </div>
+            <p class="card-info-big__temp">10 °C</p>
+            <p class="card-info-big__feels">Чувствуется как 10 °С</p>
+        </div>
     </div>
 </template>
 
--- src/components/pages/PageWeather.vue
+++ src/components/pages/PageWeather.vue
@@ -1,15 +1,39 @@
 <template>
     <div class="weather">
-        <ButtonComponent></ButtonComponent>
-        <SelectComponent
-            :options="selectOptionsWeather"
-        />
-        <br>
-        <CardInfoComponent></CardInfoComponent>
-        <br>
-        <SearchComponent></SearchComponent>
-        <br>
-        <CardInfoBigComponent></CardInfoBigComponent>
+        <div class="weather__left">
+            <div class="weather__action">
+                <div class="weather__action-left">
+                    <SearchComponent class="weather__search"/>
+                    <ButtonComponent
+                        icon="search"
+                        class="weather__btn"
+                    />
+                    <ButtonComponent
+                        icon="settings"
+                        class="weather__btn"
+                    />
+                </div>
+                <SelectComponent
+                    :options="selectOptionsWeather"
+                />
+            </div>
+            <div class="weather__card">
+                <CardInfoComponent />
+                <CardInfoComponent />
+                <CardInfoComponent />
+                <CardInfoComponent />
+                <CardInfoComponent />
+                <CardInfoComponent />
+            </div>
+
+
+        </div>
+
+        <div class="weather__right">
+            <CardInfoBigComponent></CardInfoBigComponent>
+        </div>
+
+
     </div>
 </template>
 
